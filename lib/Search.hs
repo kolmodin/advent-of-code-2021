33f549b87072154ddada7195fca5d559f286452f
Lennart Kolmodin <kolmodin@gmail.com>
2021-12-16 20:51:30 +0100

    Define dijkstra in terms of astar.

--- lib/Search.hs
+++ lib/Search.hs
@@ -21,23 +21,8 @@ dfsWith initS rep next = go mempty [initS]
 
 {-# INLINE dijkstraWith #-}
 dijkstraWith :: Ord r => s -> (s -> r) -> (s -> [(Int, s)]) -> [(s, Int)]
-dijkstraWith initS rep next = go Set.empty (PQ.singleton 0 (initS, 0))
-  where
-    go seen0 queue0 =
-      case PQ.minView queue0 of
-        Nothing -> []
-        Just ((s0, cost0), q')
-          | Set.member r seen0 -> go seen0 q'
-          | otherwise -> (s0, cost0) : go seen' q''
-          where
-            r = rep s0
-            seen' = Set.insert r seen0
-            q'' = foldl' (\q (k, v) -> PQ.insert k v q) q' nextq
-            nextq =
-              [ (cost', (s', cost'))
-                | (cost1, s') <- next s0,
-                  let !cost' = cost0 + cost1
-              ]
+dijkstraWith initS rep next =
+  astarWith initS rep (\s -> [(0, c, s') | (c, s') <- next s])
 
 {-# INLINE astarWith #-}
 astarWith :: Ord r => s -> (s -> r) -> (s -> [(Int, Int, s)]) -> [(s, Int)]
