7bd273336eeabde970640d4784da505cde333ba6
Lennart Kolmodin <kolmodin@gmail.com>
2021-12-30 16:14:44 +0100

    Day 12: count number of ways, don't enumerate them.
    Also change to Int for Cave, >= 0 for big and <0 for small nodes.
    Runtime 133ms -> 39ms.

--- bins/Day12.hs
+++ bins/Day12.hs
@@ -3,29 +3,39 @@
 module Main (main) where
 
 import Control.Monad (guard)
-import Data.Char (isUpper)
-import Data.List (intercalate)
+import Data.Char (isUpper, ord)
+import Data.List (foldl')
 import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Input (readInputDay, splitBy)
 
-data Cave = Start | End | Small String | Big String deriving (Show, Ord, Eq)
+newtype Cave = Cave Int deriving (Eq, Ord)
+
+start, end :: Cave
+start = Cave (-1)
+end = Cave (-2)
+
+isBig :: Cave -> Bool
+isBig (Cave c) = c >= 0
 
 toCave :: String -> Cave
-toCave "start" = Start
-toCave "end" = End
+toCave "start" = start
+toCave "end" = end
 toCave str@(x : _)
-  | isUpper x = Big str
-  | otherwise = Small str
+  | isUpper x = Cave (hash str)
+  | otherwise = Cave (-(hash str))
 toCave e = error ("toCave: unrecognized input \"" ++ e ++ "\"")
+
+hash :: String -> Int
+hash = foldl' (\acc c -> acc * 256 + ord c) 0
 
 buildGraph :: [(Cave, Cave)] -> Map Cave [Cave]
 buildGraph xs =
   Map.fromListWith (++) $ do
     (a, b) <- xs
     (from, to) <- [(a, b), (b, a)]
-    guard (to /= Start)
+    guard (to /= start)
     return (from, [to])
 
 parse :: String -> [(Cave, Cave)]
@@ -34,29 +44,40 @@ parse = map (extr . splitBy '-') . lines
     extr [from, to] = (toCave from, toCave to)
     extr e = error ("parse: unknown parts: " ++ show e)
 
-walk :: Bool -> Map Cave [Cave] -> [[Cave]]
-walk visitOnlyOnce cave = go (Set.singleton Start) visitOnlyOnce [Start] Start
+walkCount :: Bool -> Map Cave [Cave] -> Int
+walkCount visitOnlyOnce cave = walk1 Set.empty visitOnlyOnce start
   where
-    go !_ _ path End = return path
+    walkAll seen didTwice heres = sum (map (walk1 seen didTwice) heres)
+    walk1 !seen !didTwice !here
+      | here == end = 1
+      | isBig here = walkAll seen didTwice fromHere
+      | alreadyHere && didTwice = 0
+      | alreadyHere = walkAll seen True fromHere
+      | otherwise = walkAll (Set.insert here seen) (didTwice || alreadyHere) fromHere
+      where
+        alreadyHere = Set.member here seen
+        fromHere = cave Map.! here
+
+walk :: Bool -> Map Cave [Cave] -> [[Cave]]
+walk visitOnlyOnce cave = go (Set.singleton start) visitOnlyOnce [start] start
+  where
+    go !_ _ path here
+      | here == end = return path
     go seen twice path here = do
       to <- cave Map.! here
       let alreadySeen = Set.member to seen
       guard (not (alreadySeen && twice))
-      let seen' = case to of
-            Small _ -> Set.insert to seen
-            _ -> seen
+      let seen' = if isBig to || alreadySeen then seen else Set.insert to seen
        in go seen' (twice || alreadySeen) (to : path) to
 
-pretty :: [Cave] -> String
-pretty = intercalate "," . map pretty1
-  where
-    pretty1 End = "end"
-    pretty1 Start = "start"
-    pretty1 (Big s) = s
-    pretty1 (Small s) = s
-
+-- Part 1: 4970
+-- Part 2: 137948
 main :: IO ()
 main = do
-  caves <- buildGraph . parse <$> readInputDay 12
-  putStrLn ("Part 1: " ++ show (length (walk True caves)))
-  putStrLn ("Part 2: " ++ show (length (walk False caves)))
+  input <- parse <$> readInputDay 12
+  let caves = buildGraph input
+  -- putStrLn ("Part 1: " ++ show (length (walk True caves)))
+  -- putStrLn ("Part 2: " ++ show (length (walk False caves)))
+
+  putStrLn ("Part 1: " ++ show (walkCount True caves))
+  putStrLn ("Part 2: " ++ show (walkCount False caves))
